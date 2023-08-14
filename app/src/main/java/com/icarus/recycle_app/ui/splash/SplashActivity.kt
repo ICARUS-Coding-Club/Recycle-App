dbd31d7e8742af41ca86c84b21d424934afcbc79
wndudwkd003 <ymail3@naver.com>
2023-08-14 20:18:36 +0900

    splash init

---
+++ app/src/main/java/com/icarus/recycle_app/ui/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.icarus.recycle_app.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.icarus.recycle_app.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}

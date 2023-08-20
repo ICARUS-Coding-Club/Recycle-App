fde4e4b9321397e2ccdb38ba6f3669fb03297fe3
wndudwkd003 <ymail3@naver.com>
2023-08-20 19:34:26 +0900

    서버 리스너 업데이트

--- app/src/main/java/com/icarus/recycle_app/ui/search/image/trash_request/TrashRequestActivity.kt
+++ app/src/main/java/com/icarus/recycle_app/ui/search/image/trash_request/TrashRequestActivity.kt
@@ -2,24 +2,74 @@ package com.icarus.recycle_app.ui.search.image.trash_request
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.icarus.recycle_app.R
 import com.icarus.recycle_app.databinding.ActivityTrashRequestBinding
+import com.icarus.recycle_app.dto.Trash
+import com.icarus.recycle_app.utils.RequestServer
+import com.icarus.recycle_app.utils.ServerConnectHelper
 
 class TrashRequestActivity : AppCompatActivity() {
     private lateinit var _binding: ActivityTrashRequestBinding
+    private val serverConnectHelper = ServerConnectHelper()
 
     private val binding get() = _binding
+
+    // 임시 데이터
+    private val testTrash : Trash = Trash(0, "김치", "음식물쓰레기", "-상해서 먹지 못하는 김치는 김장 양념을 물에 한번 헹군 후 음식물쓰레기로 버려요.\n" +
+            "\n" +
+            "-가정에서 배출하는 소량의 김치국물은 하수구로 흘려보내도 무방합니다. 다만 건더기는 걸러서 음식물쓰레기로 버려주세요.\n" +
+            "\n" +
+            "-식당 등에서 대량으로 버려야할 경우 주민센터/구청 등에 생활폐기물로 신고/배출해주세요.", "-음식물 쓰레기는 살균 처리와 고온 건조 과정을 거쳐 동물용 사료 또는 경작용 퇴비로 재활용됩니다. 그래서 쉽게 분해되지 않거나 매운 맛과 향이 강해서 사료나 퇴비로 쓰일 수 없는 음식물은 재활용이 불가능합니다.\n" +
+            "\n" +
+            "-김치 양념과 국물에는 매운 캡사이신 성분과 나트륨이 다량 함유되어 있어 사료나 퇴비로 적합하지 않습니다. 반드시 양념을 헹군 후 음식물쓰레기로 배출해주세요.  ",
+        0,
+        favorite = false,
+        isRecycle = true,
+        image = "test.png"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         _binding = ActivityTrashRequestBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
+
+        // 서버 요청 리스너 등록
+        serverConnectHelper.request = object : RequestServer {
+            override fun onSuccess(testPost: TestPost) {
+                // 데이터 가져오기 성공
+                println("Post title: ${testPost.title}")
+
+                // 임시 데이터
+                initRequestData(testTrash)
+
+                // 데이터 입력이 끝나면
+                setContentView(binding.root)
+            }
+
+            override fun onFailure() {
+                // 서버 요청에 실패 했을 때
+                setContentView(binding.root)
+            }
+
+        }
+
+        // 서버 요청 실행
+        serverConnectHelper.getPost()
 
         initListener()
 
 
+    }
 
+
+    private fun initRequestData(trash: Trash) {
+        binding.tvToolBarTitle.text = trash.name
+        binding.tvTrashTitle.text = "${trash.name} 정보"
+        binding.tvTrashThrowInfoTitle.text = "${trash.name}의 버리는 방법"
+        binding.tvTrashTipInfoTitle.text = "${trash.name}의 알아두면 좋은 점"
+
+        binding.tvTrashThrowInfo.text = "${trash.method}"
+        binding.tvTrashTipInfo.text = "${trash.etc}"
 
     }
 
--- app/src/main/java/com/icarus/recycle_app/utils/ServerConnectHelper.kt
+++ app/src/main/java/com/icarus/recycle_app/utils/ServerConnectHelper.kt
@@ -1,22 +1,40 @@
 package com.icarus.recycle_app.utils
 
 import com.icarus.recycle_app.ui.search.image.trash_request.TestPost
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
+/**
+ * retrofit api 인터페이스
+ */
 interface ApiService {
     @GET("posts/1")
     fun getPost(): Call<TestPost>
 }
 
 /**
- * 서버와 통신하기 위한 클래스
+ * 서버 응답 인터페이스
+ */
+interface RequestServer {
+    fun onSuccess(testPost: TestPost)
+    fun onFailure()
+}
+
+
+/**
+ * 쓰레기 사진을 전송하여 쓰레기 정보를 반환 받는 클래스
  */
 class ServerConnectHelper {
 
     private val apiService: ApiService
+    lateinit var request: RequestServer
 
     init {
         val retrofit = Retrofit.Builder()
@@ -27,14 +45,23 @@ class ServerConnectHelper {
         apiService = retrofit.create(ApiService::class.java)
     }
 
-    fun getPost(): TestPost? {
-        val call = apiService.getPost()
-        val response = call.execute()
+    /**
+     * 서버로부터 응답된 결과에 따라 인터페이스 실행
+     */
+    fun getPost() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = apiService.getPost()
+            val response = call.execute()
 
-        return if (response.isSuccessful) {
-            response.body()
-        } else {
-            null
+            if (response.isSuccessful) {
+                withContext(Dispatchers.Main) {
+                    request.onSuccess(response.body()!!)
+                }
+            } else {
+                withContext(Dispatchers.Main) {
+                    request.onFailure()
+                }
+            }
         }
     }
 }

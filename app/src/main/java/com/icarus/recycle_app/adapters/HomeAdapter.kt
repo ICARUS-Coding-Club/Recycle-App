14af00e65601e02c6f1457e97be0519b98f81fc1
차도훈 <cha_dh1004@naver.com>
2023-08-21 18:24:14 +0900

    테마 추가 홈화면 디자인 작업

--- app/src/main/java/com/icarus/recycle_app/adapters/HomeAdapter.kt
+++ app/src/main/java/com/icarus/recycle_app/adapters/HomeAdapter.kt
@@ -1,12 +1,12 @@
 package com.icarus.recycle_app.adapters
 
-import android.R
 import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AbsListView
 import android.widget.BaseAdapter
 import android.widget.ImageView
+import com.icarus.recycle_app.R
 import com.icarus.recycle_app.dto.Trash
 
 
@@ -35,7 +35,9 @@ class HomeAdapter(private val trashes: List<Trash>, context: Context?): BaseAdap
         } else {
             imageView = convertView as ImageView
         }
-        imageView.setImageResource(R.drawable.ic_delete)
+
+        imageView.setImageResource(R.drawable.can)
+
         return imageView
     }
 }

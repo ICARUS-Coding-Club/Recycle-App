31e6d4af392d3fa75c65155e8d163d082389a19c
wndudwkd003 <ymail3@naver.com>
2023-08-23 19:32:37 +0900

    분리수거 하면 좋은점 환경 관련 팁에 병합하기위해 삭제

--- app/src/main/java/com/icarus/recycle_app/ui/info/content/recycling_advantage/RecyclingAdvantageViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.icarus.recycle_app.ui.info.content.recycling_advantage
-
-import androidx.lifecycle.ViewModel
-
-class RecyclingAdvantageViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}

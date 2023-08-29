e3fa307e75c7426e86166111c1973d1318ec0815
Juyoung_Laptop <ymail3@naver.com>
2023-08-29 20:07:44 +0900

    재활용 마크 수정

--- app/src/main/java/com/icarus/recycle_app/dto/RecyclingSymbol.kt
+++ app/src/main/java/com/icarus/recycle_app/dto/RecyclingSymbol.kt
@@ -1,3 +1,10 @@
 package com.icarus.recycle_app.dto
 
-data class RecyclingSymbol(val innerText : String, val outerText : String, val color: String)
+data class RecyclingSymbol(
+    val innerText: String,
+    val outerText: String,
+    val color: Int,
+    val detailItem: String,
+    val detailMethod: String,
+    val noRecycle: String
+)

697f6ff3073b877a35b0f109d2dcc8b08f2471b0
도훈 차 <cha_dh1004@naver.com>
2023-09-03 00:02:48 +0900

    commit

--- app/src/main/java/com/icarus/recycle_app/adapters/CategoryResultAdapter.kt
+++ app/src/main/java/com/icarus/recycle_app/adapters/CategoryResultAdapter.kt
@@ -1,11 +1,8 @@
 package com.icarus.recycle_app.adapters
 
-import android.R
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.icarus.recycle_app.databinding.ItemCategoryBinding

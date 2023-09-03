80bc4b5ba82c0e3a09ca63e1b3c4722a3cd84756
Juyoung_Laptop <ymail3@naver.com>
2023-09-03 20:30:26 +0900

    팀 소개 추가

--- app/src/main/java/com/icarus/recycle_app/ui/setting/DeveloperInfoActivity.kt
+++ app/src/main/java/com/icarus/recycle_app/ui/setting/DeveloperInfoActivity.kt
@@ -1,16 +1,99 @@
 package com.icarus.recycle_app.ui.setting
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.icarus.recycle_app.R
 import com.icarus.recycle_app.databinding.ActivityDeveloperInfoBinding
 
 class DeveloperInfoActivity : AppCompatActivity() {
+
+    val impression = mutableMapOf<String, String>()
+    init {
+        impression["차도훈"] = "dddddddddddd"
+        impression["김주영"] = "ddddddddddddd"
+        impression["김민기"] = "dddddddddddd"
+        impression["박지홍"] = "dddddddddddd"
+        impression["최민화"] = "dddddddddddd"
+        impression["임소희"] = "dddddddddddd"
+        impression["이지훈"] = "dddddddddddd"
+        impression["차호련"] = "dddddddddddd"
+        impression["김나은"] = "dddddddddddd"
+        impression["김재겸"] = "dddddddddddd"
+        impression["이가은"] = "dddddddddddd"
+        impression["태 원"] = "dddddddddddd"
+
+    }
 
     private lateinit var binding: ActivityDeveloperInfoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDeveloperInfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val linearLayout = binding.linearLayoutForText
+
+        val layoutParams = ViewGroup.MarginLayoutParams(
+            ViewGroup.MarginLayoutParams.WRAP_CONTENT,
+            ViewGroup.MarginLayoutParams.WRAP_CONTENT
+        ).apply {
+            setMargins(
+                dpToPx(20), // Start
+                dpToPx(10), // Top
+                dpToPx(20), // End
+                0
+            )
+        }
+
+        val drawable = ContextCompat.getDrawable(this, R.drawable.custom_circle_background_gray)
+
+
+        for (item in impression) {
+
+            val nameText = TextView(this)
+            nameText.text = item.key
+
+            nameText.setPadding(
+                dpToPx(20),
+                dpToPx(15),
+                dpToPx(20),
+                dpToPx(15)
+            )
+
+            nameText.layoutParams = layoutParams
+
+            nameText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15f)
+            nameText.background = drawable
+
+            val contentText = TextView(this)
+            contentText.text = item.value
+
+            contentText.setPadding(
+                dpToPx(20),
+                dpToPx(20),
+                dpToPx(20),
+                dpToPx(20)
+            )
+
+            contentText.layoutParams = layoutParams
+
+
+            contentText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15f)
+            contentText.background = drawable
+
+            linearLayout.addView(nameText)
+            linearLayout.addView(contentText)
+        }
+
+
+
+    }
+
+    fun dpToPx(dp: Int): Int {
+        return (dp * Resources.getSystem().displayMetrics.density).toInt()
     }
 }
--- app/src/main/java/com/icarus/recycle_app/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/icarus/recycle_app/ui/setting/SettingFragment.kt
@@ -3,12 +3,14 @@ package com.icarus.recycle_app.ui.setting
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.icarus.recycle_app.R
 import com.icarus.recycle_app.databinding.FragmentSettingBinding
 
@@ -38,6 +40,22 @@ class SettingFragment : Fragment() {
         binding.clAppInfo.setOnClickListener {
             dialog.setMessage("인공지능 쓰레기 재활용 안내 애플리케이션\n\nv0.1-beta.0")
             dialog.show()
+        }
+
+        binding.clEmail.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("example@example.com")) // 수신인 주소
+                putExtra(Intent.EXTRA_SUBJECT, "제목") // 이메일 제목
+                putExtra(Intent.EXTRA_TEXT, "이메일 본문") // 이메일 본문
+            }
+
+            // 이메일 앱이 설치되어 있는지 확인
+            if (emailIntent.resolveActivity(requireActivity().packageManager) != null) {
+                startActivity(emailIntent)
+            } else {
+                Toast.makeText(this@SettingFragment.requireContext(), "이메일 앱이 설치되어 있지 않습니다.", Toast.LENGTH_SHORT).show()
+            }
         }
 
 

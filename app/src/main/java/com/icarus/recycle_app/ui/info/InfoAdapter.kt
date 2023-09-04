aa617027ae379437b736948cbc881bd113698bd0
Juyoung_Laptop <ymail3@naver.com>
2023-09-04 22:52:59 +0900

    이미지 교체

--- app/src/main/java/com/icarus/recycle_app/ui/info/InfoAdapter.kt
+++ app/src/main/java/com/icarus/recycle_app/ui/info/InfoAdapter.kt
@@ -3,9 +3,13 @@ package com.icarus.recycle_app.ui.info
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.ViewOutlineProvider
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 
 import com.icarus.recycle_app.ui.info.placeholder.PlaceholderContent.PlaceholderItem
 import com.icarus.recycle_app.databinding.ItemInfoBinding
@@ -50,6 +54,14 @@ class InfoAdapter(
         fun bind(position: Int) {
             this.binding.tvTitle.text = values[position].title
             this.binding.tvContent.text = values[position].content
+
+            Glide.with(this.binding.root)
+                .load(values[position].info)
+                .apply(RequestOptions.bitmapTransform(RoundedCorners(30)))
+                .into(this.binding.ivImage)
+
+            this.binding.ivImage.outlineProvider = ViewOutlineProvider.BACKGROUND
+            this.binding.ivImage.clipToOutline = true
         }
     }
 
